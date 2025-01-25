69ae8674bdd667fd1e363a553c894f85bd51a23d
𝓕𝓽𝓶 <167397935+fatemehsheida@users.noreply.github.com>
2025-01-26 01:27:49 +0330

    okkk

---
+++ src/app/register/page.tsx
@@ -0,0 +1,11 @@
+import Component from "../../components/ui/Auth/Register-Form";
+
+export default function Page() {
+  return (
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <Component />
+      </div>
+    </div>
+  );
+}
