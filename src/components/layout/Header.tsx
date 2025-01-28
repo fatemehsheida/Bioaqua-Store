bdb8bd017710ce36273eab0cd8d5f7a7926db328
fatemehsheida <f.sh.d5232@gmail.com>
2025-01-28 13:59:02 +0330

    Style the hamburger menu and design its appearance

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -13,8 +13,19 @@ const Header = () => {
     const isMobile = useMediaQuery({ query: '(max-width: 768px)' });
 
     return (
+      <>
+      {isMobile ? (
+        <Menu className=' w-ful'>
+          <div className='text-gray-800'>
+          item one 
+          
+          </div>
+
+        </Menu>
+      ):(<div></div>)}
       <div className="vazir md:w-[80%] h-[85px] items-center flex flex-row w-[90%] px-1
       justify-between md:px-14 pt-2 pb-1.5 mx-16">
+
             <div className="flex flex-row opacity-80 items-center justify-start">
               <button className="mx-2">
                 <IoMdSearch className="size-6 hover:size-7" />
@@ -26,16 +37,7 @@ const Header = () => {
                 <AiOutlineShoppingCart className="size-6 hover:size-7" />
               </button>
             </div>
-
-        {isMobile ? (
-          <Menu className=' w-ful'>
-            <div className='text-gray-800'>
-            item one 
-            
-            </div>
-
-          </Menu>
-        ) : (
+            {!isMobile ?  (
           <>
             <div className="flex items-center gap-10">
               <div className="text-sm">
@@ -44,8 +46,9 @@ const Header = () => {
               <img src="bioaqoua.avif" alt="Bioaqoua" className="w-40" />
             </div>
           </>
-        )}
+        ):(<div></div>)}
       </div>
+      </>
     );
   };
 
