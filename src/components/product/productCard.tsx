818f2613407d8e522da5e81465802e9afd263452
fatemehsheida <f.sh.d5232@gmail.com>
2025-02-01 23:00:34 +0330

    Adding products and some styles

--- src/components/product/productCard.tsx
+++ src/components/product/productCard.tsx
@@ -8,18 +8,36 @@ interface ProductCardProps {
 
 const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
   return (
-    <div className="">
-      <Link href={`/products/${product.code}`}>
-        <img src={product.images.main} alt={product.titleEn} />
-      </Link>
-        <h3>{product.titleFa}</h3>
+    <div className="w-full px-40 bg-[#F1F3F1]">
+      <div className="px-12 pt-9 pb-7">
+        <div className="grid grid-cols-2 justify-center items-center gap-12">
 
-      <p>{product.titleEn}</p>
-      <p>Brand: {product.brand.titleEn}</p>
-      <p>Status: {product.status}</p>
-      <p>{product.expert_reviews}</p>
-    </div>
-  );
+          <div className="w-full">
+            <h1 className="font-normal text-5xl">{product.titleFa}</h1>
+            <p>{product.titleEn}</p>
+            <p>Brand: {product.brand.titleEn}</p>
+            <p className="font-light text-lg">$7.99</p>
+          </div >
+         
+        <div>
+            <div>
+            <Link href={`/products/${product.code}`}>
+              <img src={product.images.main} alt={product.titleEn} className="object-cover h-" />
+            </Link>
+            </div>
+            <div>
+            <div className="flex flex-row">
+              {product.images.list.map((imageListURL,index) => <div key={index}>
+                <img src={imageListURL} className=" w-24 h-24"/>
+              </div>)}
+              </div>
+            </div>
+          </div>
+        </div>
+        <p dangerouslySetInnerHTML={{ __html: product.expert_reviews }}></p >
+      </div>
+      </div>
+      );
 };
 
-export default ProductCard;
+      export default ProductCard;
