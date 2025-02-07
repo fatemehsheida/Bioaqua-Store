8d293754365971db6d06439d2ca3f1aaf68529fd
fatemehsheida <f.sh.d5232@gmail.com>
2025-02-07 07:55:41 +0330

    adding filter bar

--- src/components/product/productCard.tsx
+++ src/components/product/productCard.tsx
@@ -12,33 +12,34 @@ const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
 
     <div className="px-8 py-10">
       <div className="justify-between">
-
-        <Link href={`/products/${product.code}`}>
-          <div className="w-44 h-[260px]  flex flex-col justify-start cursor-pointer group ">
-            <div className=" group-hover:rounded-3x">
-              <img src={product.images.list[0]} className=" rounded-xl group-hover:hidden object-cover w-44 h-44" />
-              <img src={product.images.list[1]} className=" rounded-xl hidden group-hover:block object-cover w-44 h-44" />
-
-            </div>
-
-            <div className="text-[#2E2A39] pb-4 pt-2">
-              <span className="flex flex-row items-start flex-1 group-hover:underline
-                     justify-start gap-3 text-sm font-noraml w-full ">
-                {product.titleFa}
-              </span>
-              <div className="gap-2 flex flex-row justify-start items-center px-4 text-sm font-normal" dir="ltr">
-                <span>
-                  $ 3.656
-                </span>
+        <div className=" group ">
+          <Link href={`/products/${product.code}`}>
+            <div className="w-44 h-[260px]  flex flex-col justify-start cursor-pointer">
+              <div className=" group-hover:rounded-3x">
+                <img src={product.images.list[0]} className=" rounded-xl group-hover:hidden object-cover w-44 h-44" />
+                <img src={product.images.list[1]} className=" rounded-xl hidden group-hover:block object-cover w-44 h-44" />
 
               </div>
+
+              <div className="text-[#2E2A39] pb-4 pt-2">
+                <span className="flex flex-row items-start flex-1 group-hover:underline
+                     justify-start gap-3 text-sm font-noraml w-full ">
+                  {product.titleFa}
+                </span>
+                <div className="gap-2 flex flex-row justify-start items-center px-4 text-sm font-normal" dir="ltr">
+                  <span>
+                    $ 3.656
+                  </span>
+
+                </div>
+              </div>
+
             </div>
 
-          </div>
 
-
-        </Link>
-        <AddToCart product={product} quantity={1} style={" w-full bg-white ring-1 ring-[#2E2A39] hover:ring-2 px-4 py-3 rounded-lg text-[#2E2A39] text-sm font-normal"} />
+          </Link>
+          <AddToCart product={product} quantity={1} style={" w-full bg-white ring-1 ring-[#2E2A39] hover:ring-2 px-4 py-3 rounded-lg text-[#2E2A39] text-sm font-normal"} />
+        </div>
       </div>
     </div >
 
@@ -46,3 +47,4 @@ const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
 };
 
 export default ProductCard;
+
--- src/components/product/productList.tsx
+++ src/components/product/productList.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ProductCard from './productCard';
 import { Product } from "@/types/type";
+import FilterBar from "./FilterBar";
 
 interface ProductListProps {
   products: Product[];
@@ -9,6 +10,7 @@ interface ProductListProps {
 const ProductList: React.FC<ProductListProps> = ({ products }) => {
   return (
     <div className="product-list w-full bg-[#F1F3F1] flex flex-row flex-wrap lg:px-40 2xl:px-72 items-center justify-center ">
+      <FilterBar />
       {products.map((product: Product) => (
         <ProductCard key={product.id} product={product} />
       ))}
