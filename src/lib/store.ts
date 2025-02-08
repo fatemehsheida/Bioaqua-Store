f693430b414dd3674620f26d60b363250f67a2b4
fatemehsheida <f.sh.d5232@gmail.com>
2025-02-08 16:22:39 +0330

    feat: add remove item from cart

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,3 +1,27 @@
+// import { cartSlice } from "./slice";
+// import { configureStore, createStore } from '@reduxjs/toolkit';
+// import { persistStore, persistReducer } from 'redux-persist'
+// import storage from 'redux-persist/lib/storage'
+
+
+
+
+// const persistConfig = {
+//     key: 'cart',
+//   }
+   
+//   const persistedReducer = persistReducer(persistConfig, cartSlice.reducer)
+   
+
+
+// export  const store = configureStore({
+//     reducer:  {'cart' : persistedReducer},
+    
+    
+// })
+
+// export const persistor = persistStore(store)
+
 import { cartSlice } from "./slice";
 import { configureStore } from '@reduxjs/toolkit';
 
@@ -8,3 +32,7 @@ export  const store = configureStore({
         cart : cartSlice.reducer
     }
 })
+<<<<<<< Updated upstream
+=======
+export type RootState = ReturnType<typeof store.getState>
+>>>>>>> Stashed changes
