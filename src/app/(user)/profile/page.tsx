8b721a90d78976e970ca697b52e8ced525869a5c
fatemehsheida <f.sh.d5232@gmail.com>
2025-02-10 22:36:03 +0330

    feat: The basic structure of the profile

---
+++ src/app/(user)/profile/page.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const Profile = () => {
+    return (
+        <div className=''>
+            <div className='right'></div>
+
+
+
+            <div className='left'>
+                <div>
+
+                    <div><img src="" alt="" /></div>
+                    <div>
+                        <span>firstname</span>
+                        <span>lastname</span>
+                        <span>email</span>
+                        <span>address</span>
+                    </div>
+                </div>
+
+
+                <div>
+                    <button>order item</button>
+                    <button>status</button>
+                </div>
+
+                <div>
+                    <button>ok</button>
+                </div>
+            </div>
+
+
+        </div>
+    )
+}
+
+export default Profile
