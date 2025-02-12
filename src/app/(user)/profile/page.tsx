623b8ca1c2e823a0bd4d30fe8ee58b94fb5c929c
fatemehsheida <f.sh.d5232@gmail.com>
2025-02-12 18:34:30 +0330

    refactor: redesigin order Item component structures

--- src/app/(user)/profile/page.tsx
+++ src/app/(user)/profile/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 import AccordionOrder from '@/components/profile/AccordionOrder';
+import OrderItem from '@/components/profile/OrderItem';
 import ProfileUser from '@/components/profile/profile';
 import React from 'react'
 import { useMediaQuery } from 'react-responsive';
@@ -15,39 +16,8 @@ const Profile = () => {
                 <AccordionOrder />
             ) : (<div></div>)}
             {!isMobile ? (
-                <div className='right w-1/2 h-full flex flex-col justify-start items-start pt-10 pl-6'>
-                    <div className='w-full'>
-                        <div className='flex flex-row justify-between w-full pb-2'>
-                            <h3 className="text-lg font-medium mt-6 mb-2">تاریخچه سفارشات</h3>
-                            <div className='flex gap-4 justify-center items-center'>
-                                <button className='bg-transparent transition duration-500 hover:ring-2 hover:ring-[#94D2BD]
-                                     text-gray-700 text-xs px-5 py-3 w-1/8 rounded-lg font-semibold'>مشاهده جزئیات</button>
-                            </div>
-                        </div>
-                        <div className="space-y-4">
-
-                            <div className="border border-gray-200 rounded-md p-4 flex gap-9">
-                                <h4 className="text-base font-medium">سفارش شماره: #12345</h4>
-                                <p className="text-sm text-gray-600">تاریخ: 13 آبان 1403</p>
-                                <p className="text-sm text-gray-600">وضعیت: <span className="font-bold text-green-600">تحویل داده شده</span></p>
-                                <p className="text-sm text-gray-600">جمع کل: 500,000 تومان</p>
-                            </div>
-
-                            <div className="border border-gray-200 rounded-md p-4 flex gap-9">
-                                <h4 className="text-base font-medium">سفارش شماره: #67890</h4>
-                                <p className="text-sm text-gray-600">تاریخ: 20 بهمن 1403</p>
-                                <p className="text-sm text-gray-600">وضعیت: <span className="font-bold text-yellow-600">در حال پردازش</span></p>
-                                <p className="text-sm text-gray-600">جمع کل: 800,000 تومان</p>
-                            </div>
-
-                        </div>
-                    </div>
-                </div>
-
+                <OrderItem />
             ) : (<div></div>)}
-
-
-
         </div>
     )
 }
