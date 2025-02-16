5dd5c4c28216d9f7244aed80bcbcebbb3a358eac
fatemehsheida <f.sh.d5232@gmail.com>
2025-02-16 22:29:18 +0330

    feat: add default dark mode settings

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -16,9 +16,12 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+
+
   return (
     <ReduxProvider>
-      <html lang="fn" dir="rtl" className="h-screen w-full items-center">
+      <html lang="fn" dir="rtl" className="h-screen w-full items-center dark">
         <head>
           <meta charSet="utf-8" />
           <link href="../style.css" rel="stylesheet"></link>
@@ -27,9 +30,10 @@ export default function RootLayout({
         <body className=" bg-gradient-to-bl from-[#6BB0A9] from-4% via-[#CCC6B0] to-[#F0D6C0]
           transition-all duration-5 bg-fixed flex flex-col justufy-center items-center">
           <Header />
+          
           {children}
           <div className="flex flee-col text-sm md:mx-auto md:w-[65%] justify-between 
-        py-10 md:gap-10 text-gray-700 font-light">
+        py-10 md:gap-10 text-gray-700 font-light ">
             <Footer />
             <div>
               <img src="http://localhost:8000/uploads/image-1738349698115-546474799.avif" alt="Bioaqoua" className="md:w-40 md:mr-24 w-52" />
